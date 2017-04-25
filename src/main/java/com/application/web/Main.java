6e75bbac3ddd99221c0b220496a44c2e689f5992
jamesrloveday <jamesrloveday@outlook.com>
2017-04-25 23:03:07 +0100

    Added a from submit example

--- src/main/java/com/application/web/Main.java
+++ src/main/java/com/application/web/Main.java
@@ -2,9 +2,15 @@ package com.application.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.post;
 
 public class Main {
 
@@ -16,5 +22,14 @@ public class Main {
         get("/health", ((request, response) -> "Alive"));
         log.info("Healthcheck requested");
         get(Path.Web.INDEX, IndexController.serveIndexPage);
+
+        //form submit example
+        get("/form/submit", ((request, response) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("", "");
+            return new ThymeleafTemplateEngine().render(new ModelAndView(model, "/pages/formsubmit"));
+        }));
+
+        post("/form/submit", ((request, response) -> "Hello " + request.queryParams("name")));
     }
 }
